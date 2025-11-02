174704fdda0e38758f36a1d20c615c45ab1ca943
megustatraffic-crypto <megustatraffic@gmail.com>
2025-11-02 17:52:47 +0200

    User.js

--- models/User.js
+++ models/User.js
@@ -1,27 +1,43 @@
 // models/User.js
 import db from '../db.js';
 
-class UserModel {
-  static async findOne(query) {
-    const keys = Object.keys(query);
-    return db.data.users.find(u => keys.every(k => u[k] == query[k])) || null;
+function shallowMatch(obj, query){
+  return Object.keys(query).every(k => String(obj[k]) === String(query[k]));
+}
+
+class User {
+  static async findOne(query){
+    await db.read();
+    return db.data.users.find(u => shallowMatch(u, query)) || null;
   }
 
-  static async create(obj) {
-    db.data.users.push(obj);
+  static async create(obj){
+    await db.read();
+    const toCreate = Object.assign({
+      user_id: (obj.user_id || obj.telegramId || ("u"+Date.now())),
+      name: obj.name || "Guest",
+      frags: 0, level:1, drones:0, farm: Array(15).fill({status:"empty"}),
+      profile: obj.profile || { name: obj.name || "Guest", avatar: obj.avatar || "" },
+      quests: obj.quests || {}
+    }, obj);
+    db.data.users.push(toCreate);
     await db.write();
-    return obj;
+    return toCreate;
   }
 
-  static async findOneAndUpdate(query, update) {
-    const keys = Object.keys(query);
-    const user = db.data.users.find(u => keys.every(k => u[k] == query[k]));
-    if (!user) return null;
-
-    Object.assign(user, update);
+  static async findOneAndUpdate(query, update){
+    await db.read();
+    const u = db.data.users.find(x => shallowMatch(x, query));
+    if(!u) return null;
+    Object.assign(u, update);
     await db.write();
-    return user;
+    return u;
+  }
+
+  static async all(){
+    await db.read();
+    return db.data.users;
   }
 }
 
-export default UserModel;
+export default User;
