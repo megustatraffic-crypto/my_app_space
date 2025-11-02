67c67f6867e26257652719aa4e6b424f50696466
megustatraffic-crypto <megustatraffic@gmail.com>
2025-11-02 17:23:51 +0200

    service-worker.js

---
+++ frontend/service-worker.js
@@ -0,0 +1,20 @@
+const CACHE="cosmic-farm-v1";
+const FILES=[
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./api.js",
+  "./telegram-init.js",
+  "./manifest.json",
+  "./icon192.png",
+  "./icon512.png"
+];
+
+self.addEventListener("install",e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
+});
+
+self.addEventListener("fetch",e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
