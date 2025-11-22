f0a1db053bf25272229a7039934cdae8a73c565b
megustatraffic-crypto <megustatraffic@gmail.com>
2025-11-22 10:41:49 +0200

    db.js

--- db.js
+++ db.js
@@ -1,9 +1,17 @@
-{
-  "users": [],
-  "planetProgress": {},
-  "resources": {},
-  "factories": {},
-  "settings": {
-    "version": 1
-  }
-}
+import { Low } from 'lowdb'
+import { JSONFile } from 'lowdb/node'
+
+// путь к базе
+const adapter = new JSONFile('./db.json')
+const db = new Low(adapter, {
+  users: [],
+  planetProgress: {},
+  resources: {},
+  factories: {},
+  settings: { version: 1 }
+})
+
+await db.read()
+await db.write()
+
+export default db
